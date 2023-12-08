6da814a9ae3fa4a9fcd823d73c9b5ab0fcd5ca43
benjamin <benjamin.edgard@gmail.com>
2023-12-08 01:07:39 +0100

    changement

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,28 +1,32 @@
 function Collapse() {
     const aboutDatas = [
         {
-            "id": "1",
+            id: 1,
             "title": "Fiabilité",
             "content": "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
         },
         {
-            "id": "2",
+            id: 2,
             "title": "Respect",
             "content": "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.."
         },
         {
-            "id": "3",
-            "title": "Service",
-            "content": "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.."
+            id: 3,
+            title: "Service",
+            content: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.."
         },
         {
-            "id": "4",
-            "title": "Sécurité",
-            "content": "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+            id: 4,
+            title: "Sécurité",
+            content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
         }
     ]
+
+
+
+
         return (
-            aboutDatas.map((data=> {
+            aboutDatas.map((data)=> {
                 return ( 
                 <div className='description__about'>
                 <details>
@@ -35,7 +39,7 @@ function Collapse() {
                 </details>
             </div> )
     
-            })))
+            }))
     
 }
 
