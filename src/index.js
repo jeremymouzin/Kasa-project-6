51278a75ae5b63f3e7d97974e2dbbab71aaedf00
benjamin <benjamin.edgard@gmail.com>
2023-12-17 22:32:06 +0100

    router

--- src/index.js
+++ src/index.js
@@ -6,31 +6,39 @@ import ErrorPage from './components/Error';
 import About from './pages/about';
 import Home from './pages/home';
 import Flat from './pages/flat';
+import datasFlat from './assets/data.json'
+
+const data = datasFlat.map(data => console.log(data))
+
+const router = createBrowserRouter(
+
+  [
 
 
+    {
+      path: '/',
+      element: <Home />,
+      errorElement: <ErrorPage />,
+    },
 
-const router = createBrowserRouter([
+    {
+      path: "/about",
+      element: <About />
+    },
+
+    {
+      path: "/flat",
+      element: <Flat />
+
+    },
+
+    {
+      path: `/flat/:${data.id}`,
+      element: <Flat />
+    }
 
 
-  {
-    path: '/',
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-
-  {
-    path: "/about",
-    element: <About />
-  },
-
-  {
-    path: "/flat",
-    element: <Flat />
-
-  }
-
-
-])
+  ])
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
